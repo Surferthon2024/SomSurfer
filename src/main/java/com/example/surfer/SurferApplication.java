7f7afb4804aeea60c78bc4b3affc5a574f94c74b
chaechaeyeon <00dreamadream@kakao.com>
2024-08-19 16:35:13 +0900

    [init] project init

---
+++ src/main/java/com/example/surfer/SurferApplication.java
@@ -0,0 +1,13 @@
+package com.example.surfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/surfer/SurferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.surfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
