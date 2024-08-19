6921e34b53b5e59347c6a2a6967bd1116d715c92
karam Choi <riverchoi2523@gmail.com>
2024-08-19 23:01:04 +0900

    [FIX] 일정 등록 및 삭제

--- src/main/java/com/example/surfer/repository/ScheduleRepository.java
+++ src/main/java/com/example/surfer/repository/ScheduleRepository.java
@@ -1,23 +1,27 @@
 package com.example.surfer.repository;
 
 import com.example.surfer.dto.ScheduleDto;
-import com.example.surfer.service.ScheduleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class ScheduleRepository {
-    @Autowired
-    private ScheduleService scheduleService;
+    private List<ScheduleDto> schedules = new ArrayList<>();
+
+    public List<ScheduleDto> getSchedules(){
+        return schedules;
+    }
 
     public ScheduleDto createSchedule(String title, String startD, String endD) {
         ScheduleDto scheduleDto = new ScheduleDto(title, startD, endD);
-        scheduleService.getSchedules().add(scheduleDto);
+        schedules.add(scheduleDto);
 
         return scheduleDto;
     }
 
     public void deleteSchedule(int index) {
-        scheduleService.getSchedules().remove(index);
+        schedules.remove(index);
     }
 }
--- src/main/java/com/example/surfer/service/ScheduleService.java
+++ src/main/java/com/example/surfer/service/ScheduleService.java
@@ -12,21 +12,18 @@ import java.util.List;
 public class ScheduleService {
     @Autowired private ScheduleRepository scheduleRepository;
 
-    private static final ScheduleService instance = new ScheduleService();
-    private List<ScheduleDto> schedules = new ArrayList<>();
-
-    public ScheduleService getInstance() {
-        return instance;
-    }
-
-    public List<ScheduleDto> getSchedules() {
-        return schedules;
+    public List<ScheduleDto> getSchedules(){
+        return scheduleRepository.getSchedules();
     }
 
     public List<ScheduleDto> createSchedules(int index){
         List<String> plans = new ArrayList<>();
         //index로 본문을 알아내서 기간추출 api 호출 -> 추출결과 plans에 저장
 
+        //test
+        plans.add("수강신청|2024-08-24|2024-08-26");
+
+        List<ScheduleDto> result = new ArrayList<>();
         for(String plan : plans){
             String[] str = plan.split("|");
 
@@ -35,9 +32,11 @@ public class ScheduleService {
                 scheduleDto = scheduleRepository.createSchedule(str[0], str[1], str[2]);
             else
                 scheduleDto = scheduleRepository.createSchedule(str[0], str[1], str[1]);
+
+            result.add(scheduleDto);
         }
 
-        return schedules;
+        return result;
     }
 
     public String deleteSchedule(int index){
